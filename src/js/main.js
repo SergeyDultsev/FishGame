94f7e78c9951ff7d01ec034f9c9665c56b9e67d9
SergeyDultsev <dultsev010904@gmail.com>
2025-09-27 13:03:02 +0700

    feat: refactoring, add gitignore and README

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,31 @@
 const playersManager = new PlayersManager();
 const gameManager = new GameManager();
 
-// Функция-объект игрока
 function Players(username, sumPoint) {
     this.playerName = username;
     this.point = sumPoint;
 }
 
-// Объект-менеджер игрока
 function PlayersManager() {
     this.players = [];
 
-    // Функция принимает значение из формы, добавляет в массив и сбрасывает форму
     this.addPlayer = (player) => {
         this.players.push(player);
-        this.sortPlayer()
         this.renderPlayerList();
     };
 
-    // Рендер информации о игроках
     this.renderPlayerList = () => {
         const playerList = document.querySelector(".player-list");
         playerList.innerHTML = "";
 
-        let bestPlayer = this.players.slice(0, 10);
+        const sortedPlayers = this.players.sort((a, b) => b.point - a.point);
+        let bestPlayer = sortedPlayers.slice(0, 10);
 
         bestPlayer.forEach((player, index) => {
             playerList.insertAdjacentHTML("beforeEnd", this.renderPlayerTemplate(player, index));
         });
     };
 
-    // Рендер данных игроков после игры
     this.renderPlayerTemplate = (player, index) => {
         return `
             <div class="player-border-top"></div>
@@ -43,16 +38,8 @@ function PlayersManager() {
             </article>
         `;
     };
-
-    // Сортировка игроков по очкам
-    this.sortPlayer = () => {
-        this.players.sort(function(a, b) {
-            return b.point - a.point;
-        });
-    }
 }
 
-// Функция принимает данные о игроке
 function getPlayerData() {
     const playerNameInput = document.getElementById("player-name");
 
@@ -63,37 +50,30 @@ function getPlayerData() {
     gameManager.sumPoint = 0;
 }
 
-// Функции для игры
+
 function GameManager(){
     this.stateGame = false;
-    this.timeInSeconds = 60; // Исходное значение времени: 1 минута
+    this.timeInSeconds = 60;
     this.sumPoint = 0;
     this.numberOfFish = 0;
-
-    //Интервалы для время и рыб
     this.fishInterval;
     this.timeInterval;
 
-    // Функция для обновления времени и проверки завершения игры
     this.updateTime = () => {
-        if(this.stateGame){
+        if (this.stateGame){
             const timeElement = document.querySelector(".check-item__time");
 
-            // Обновляем отображаемое время
             const minutes = Math.floor(this.timeInSeconds / 60);
             const seconds = this.timeInSeconds % 60;
             timeElement.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
             this.timeInSeconds--;
     
-            if (minutes === 0 && seconds === 0) {
-                this.gameOver();
-            }
-        }else{
+            if (minutes === 0 && seconds === 0) this.gameOver();
+        } else{
             return false;
         }
     };
 
-    //Сброс игры
     this.resetGame = () => {
         const seconds = this.timeInSeconds = 60;
         const minutes = Math.floor(this.timeInSeconds / 60);
@@ -102,7 +82,6 @@ function GameManager(){
         document.querySelector(".check-item__point").innerHTML = 0;
     };
 
-    // Увеличение баллов
     this.increasePoints = (points) => {
         const pointElement = document.querySelector(".check-item__point");
         if(this.stateGame){
@@ -113,8 +92,7 @@ function GameManager(){
             }
         }
     };
-    
-    // Конец игры
+
     this.gameOver = () => {
         this.stateGame = false;
 
@@ -154,19 +132,16 @@ document.querySelector(".form-player").addEventListener("click", function (event
     }
 });
 
-// Рендер рыб
 function renderFish() {
-    if(gameManager.stateGame){
+    if (gameManager.stateGame) {
         const gameArea = document.querySelector(".game-area");
         for (let i = 0; i < gameManager.numberOfFish; i++) {
             let maxWidth = gameArea.innerWidth - 60;
             let maxHeight = gameArea.clientHeight - 60;
-    
-            // Генерация случайных координат в пределах размеров игровой области
+
             let x = Math.floor(Math.random() * maxWidth) + 1;
             let y = Math.floor(Math.random() * maxHeight) + 1;
-    
-            // Генерация случайных размеров в пределах [10, 20, 30, 40]
+
             let point = [10, 20, 30, 40][Math.floor(Math.random() * 4)];
             let widthFish = point;
             let heightFish = point;
@@ -175,24 +150,19 @@ function renderFish() {
             fish.className = 'fish';
             fish.dataset.points = point;
 
-            // Изображения рыбы
             fish.style.backgroundImage = `url('./src/img/general-images/fish.png')`;
-            
-            // Установка координат
+
             fish.style.left = x + 'px';
             fish.style.top = y + 'px';
 
-            // Установка размеров рыбы
             fish.style.width = widthFish + 'px';
             fish.style.height = heightFish + 'px';
 
-            // Добавление элемента в DOM
             gameArea.appendChild(fish);
             moveFish(fish);
             gameManager.numberOfFish = 0;
         }
 
-        // Обработчик клика по рыбе
         gameArea.addEventListener("click", function(event) {
             const clickedFish = event.target.closest('.fish');
     
@@ -210,17 +180,15 @@ function renderFish() {
     }
 }
 
-// Анимация движения рыбы
 function moveFish(fish) {
     if (gameManager.stateGame) {
         const gameArea = document.querySelector(".game-area");
         let left = parseFloat(fish.style.left) || 0;
 
         function animate() {
-            left += 2; // Изменение координаты
+            left += 2;
             fish.style.left = left + 'px';
 
-            // Прерываем анимацию, если рыба вышла за границы воды
             if (left + fish.clientWidth > gameArea.clientWidth) {
                 fish.remove();
                 return;
@@ -229,15 +197,11 @@ function moveFish(fish) {
             requestAnimationFrame(animate);
         }
 
-        // Запускаем анимацию
         animate();
     }
 }
 
-// Чистка рыб при окончании игры с игрового поля
 function clearSea() {
     const gameArea = document.querySelector(".game-area");
-    while (gameArea.firstChild) {
-        gameArea.removeChild(gameArea.firstChild);
-    }
+    while (gameArea.firstChild) gameArea.removeChild(gameArea.firstChild);
 }
