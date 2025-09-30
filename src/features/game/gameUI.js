a921c9621acb5d21f8dad124e226adb8060f1f56
SergeyDultsev <dultsev010904@gmail.com>
2025-09-30 22:19:26 +0700

    feat: redesign

--- src/features/game/gameUI.js
+++ src/features/game/gameUI.js
@@ -108,11 +108,11 @@ export class GameUI {
         players.forEach((player, id) => {
             playerList.insertAdjacentHTML('beforeend', `
                 <article class="player-item">
-                    <div class="player-item__left">
-                        <p id="player-id" class="player-data">${id + 1}</p>
+                    <p id="player-id" class="player-data">${id + 1}</p>
+                    <div class="player-item__data">
                         <p id="player-name" class="player-data">${player.name}</p>
+                        <p id="player-point" class="player-data">${player.points}</p>
                     </div>
-                    <p id="player-point" class="player-data">${player.points}</p>
                 </article>
             `);
         });
